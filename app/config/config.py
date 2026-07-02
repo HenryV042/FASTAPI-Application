820672feb33f3501fced05b1df95e711497e062f
José Henry Viana Saboia <henry.viana.dev@gmail.com>
2026-07-02 02:02:15 -0300

    fix(config): altera o nome da Classe

--- app/config/config.py
+++ app/config/config.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv() # Carrega o .env
 
-class Sentings:
+# Associa os valores do .env para a conexão com o banco (sem valor padrão) 
+class Settings:
     db_host: str = os.getenv("DB_HOST")
     db_name: str = os.getenv("DB_NAME")
     db_user: str = os.getenv("DB_USER")
     db_password: str = os.getenv("DB_PASSWORD")
 
-settings = Sentings()
+settings = Settings()
