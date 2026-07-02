9009e267274a8ee4ec64e6144de7b342e0b0bb8e
José Henry Viana Saboia <henry.viana.dev@gmail.com>
2026-07-02 01:58:59 -0300

    feat(model): adiciona model de animal

---
+++ app/model/animal_model.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+class Animal (BaseModel):
+    id_animal: int
+    id_fazenda: int
+    id_raca: int
+    nome: str
+    sexo: str
+    data_nasc: Optional[str] = None
+    peso: Optional[float] = None
+    status_saude: Optional[str] = None
+
+class AnimalUpdate (BaseModel):
+    nome: str
+    sexo: str
+    data_nasc: Optional[str] = None
+    peso: Optional[float] = None
+    status_saude: Optional[str] = None
